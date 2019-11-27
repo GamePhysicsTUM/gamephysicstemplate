051fb51163d2c2b4bf15e1b5e55c877fff5238ac
sirin <ge75piq@mytum.de>
2019-11-27 22:00:49 +0100

    Demo2

--- Simulations/RigidBodySystem.cpp
+++ Simulations/RigidBodySystem.cpp
@@ -13,7 +13,7 @@ void RigidBodySystem::init(Vec3 position, Vec3 size, float mass)
 	
 	this->comPosition = position;
 	this->angularMomentum = Vec3(0, 0, 0);
-	this->orientation = Quat();
+	this->orientation = Quat(0,0,0,1);
 }
 
 void RigidBodySystem::precalculateIntertiaInverse()
@@ -21,7 +21,11 @@ void RigidBodySystem::precalculateIntertiaInverse()
 	float xx = 12 / (mass * (pow(size.y, 2) + pow(size.z, 2)));
 	float yy = 12 / (mass * (pow(size.x, 2) + pow(size.z, 2)));
 	float zz = 12 / (mass * (pow(size.x, 2) + pow(size.y, 2)));
-
+	/*
+	float xx = pow(size.x, 2) * 8;
+	float yy = pow(size.y, 2) * 8;
+	float zz = pow(size.z, 2) * 8;
+	*/
 	intertiaTensorInverse = Mat4(xx, 0,0,0,
 		0,yy,0,0,
 		0,0, zz,0,
--- Simulations/RigidBodySystemSimulator.cpp
+++ Simulations/RigidBodySystemSimulator.cpp
@@ -2,10 +2,9 @@
 
 RigidBodySystemSimulator::RigidBodySystemSimulator()
 {
-	m_externalForce = Vec3(0,0,0);
+	m_externalForce = Vec3(0, 0, 0);
 	torque = Vec3(0, 0, 0);
 	count = 0;
-	
 }
 
 const char * RigidBodySystemSimulator::getTestCasesStr()
@@ -28,13 +27,42 @@ void RigidBodySystemSimulator::reset()
 
 void RigidBodySystemSimulator::drawFrame(ID3D11DeviceContext * pd3dImmediateContext)
 {
-	DUC->drawRigidBody(Mat4(m_pRigidBodySystem[0].size.x, 0, 0, 0,
-		0, m_pRigidBodySystem[0].size.y, 0, 0,
-		0, 0, m_pRigidBodySystem[0].size.z, 0,
-		0, 0, 0, 1) * m_pRigidBodySystem[0].orientation.getRotMat() + Mat4(0, 0, 0, m_pRigidBodySystem[0].comPosition.x,
-			0, 0, 0, m_pRigidBodySystem[0].comPosition.y,
-			0, 0, 0, m_pRigidBodySystem[0].comPosition.z,
-			0, 0, 0, 0));
+	Mat4 objToWorld;
+	switch (m_iTestCase)
+	{
+	case 0:
+		DUC->drawRigidBody(
+			Mat4(m_pRigidBodySystem[0].size.x, 0, 0, 0,
+				0, m_pRigidBodySystem[0].size.y, 0, 0,
+				0, 0, m_pRigidBodySystem[0].size.z, 0,
+				0, 0, 0, 1) *
+			m_pRigidBodySystem[0].orientation.getRotMat() *
+			Mat4(1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				m_pRigidBodySystem[0].comPosition.x, m_pRigidBodySystem[0].comPosition.y, m_pRigidBodySystem[0].comPosition.z, 1));
+		break;
+	case 1:
+		objToWorld = Mat4(m_pRigidBodySystem[1].size.x, 0, 0, 0,
+			0, m_pRigidBodySystem[1].size.y, 0, 0,
+			0, 0, m_pRigidBodySystem[1].size.z, 0,
+			0, 0, 0, 1) *
+			m_pRigidBodySystem[1].orientation.getRotMat() *
+			Mat4(1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				m_pRigidBodySystem[1].comPosition.x, m_pRigidBodySystem[1].comPosition.y, m_pRigidBodySystem[1].comPosition.z, 1);
+		DUC->drawRigidBody(objToWorld);
+		DUC->drawSphere(Vec3(0, 0, 0), Vec3(0.01f, 0.01f, 0.01f));
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	default:
+		break;
+	}
+
 
 }
 
@@ -45,19 +73,19 @@ void RigidBodySystemSimulator::notifyCaseChanged(int testCase)
 	{
 	case 0:
 		addRigidBody(Vec3(0, 0, 0), Vec3(1, 0.6f, 0.5f), 2);
-		m_externalForce = Vec3(1, 1, 0);
-		torque = cross(Vec3(0.3f, 0.5f, 0.25f), Vec3(1, 1, 0));
 		setOrientationOf(0, Quat(0, 0, sqrt(2) / 2, sqrt(2) / 2));
+		applyForceOnBody(0, Vec3(0.3f, 0.5f, 0.25f), Vec3(1, 1, 0));
 		IntegrateRigidBody(0, 2);
 		break;
 	case 1:
+		addRigidBody(Vec3(-0.2f, -0.1f, 0.1f), Vec3(0.2, 0.6f, 0.5f), 2);
+
 		break;
 	case 2:
 		break;
 	case 3:
 		break;
 	default:
-		cout << "Empty Test!\n";
 		break;
 	}
 }
@@ -69,7 +97,7 @@ void RigidBodySystemSimulator::externalForcesCalculations(float timeElapsed)
 		mouseDiff.x = m_trackmouse.x - m_oldtrackmouse.x;
 		mouseDiff.y = m_trackmouse.y - m_oldtrackmouse.y;
 
-		Vec3 contact = Vec3(m_trackmouse.x, m_trackmouse.y, 0);
+		Vec3 contact =  - Vec3(m_oldtrackmouse.x, m_oldtrackmouse.y, 0);
 
 		if (mouseDiff.x != 0 || mouseDiff.y != 0)
 		{
@@ -79,9 +107,9 @@ void RigidBodySystemSimulator::externalForcesCalculations(float timeElapsed)
 			Vec3 inputWorld = worldViewInv.transformVectorNormal(inputView);
 
 			contact = worldViewInv.transformVectorNormal(contact);
-			
+
 			// find a proper scale!
-			float inputScale = 0.0005f;
+			float inputScale = 0.001f;
 			contact = contact * inputScale;
 			inputWorld = inputWorld * inputScale;
 			m_externalForce += inputWorld;
@@ -92,8 +120,9 @@ void RigidBodySystemSimulator::externalForcesCalculations(float timeElapsed)
 
 void RigidBodySystemSimulator::simulateTimestep(float timeStep)
 {
+
 	if (m_iTestCase == 1) {
-			IntegrateRigidBody(0, timeStep);
+		IntegrateRigidBody(1, timeStep);
 	}
 }
 
@@ -113,26 +142,34 @@ void RigidBodySystemSimulator::onMouse(int x, int y)
 
 int RigidBodySystemSimulator::getNumberOfRigidBodies()
 {
-	return 0;
+	return count;
 }
 
 Vec3 RigidBodySystemSimulator::getPositionOfRigidBody(int i)
 {
-	return Vec3();
+	return m_pRigidBodySystem[i].comPosition;
 }
 
 Vec3 RigidBodySystemSimulator::getLinearVelocityOfRigidBody(int i)
 {
-	return Vec3();
+	return  m_pRigidBodySystem[i].comVelocity;
 }
 
 Vec3 RigidBodySystemSimulator::getAngularVelocityOfRigidBody(int i)
 {
-	return Vec3();
+	return m_pRigidBodySystem[i].angularVelocity;
 }
 
 void RigidBodySystemSimulator::applyForceOnBody(int i, Vec3 loc, Vec3 force)
 {
+	m_externalForce = force;
+	Vec3 objloc = (m_pRigidBodySystem[i].orientation.getRotMat() *
+		Mat4(1, 0, 0, 0,
+			0, 1, 0, 0,
+			0, 0, 1, 0,
+			m_pRigidBodySystem[i].comPosition.x, m_pRigidBodySystem[i].comPosition.y, m_pRigidBodySystem[i].comPosition.z, 1)).inverse().transformVector(loc);
+	torque = cross(objloc, force);
+
 }
 
 void RigidBodySystemSimulator::addRigidBody(Vec3 position, Vec3 size, int mass)
@@ -148,24 +185,41 @@ void RigidBodySystemSimulator::setOrientationOf(int i, Quat orientation)
 
 void RigidBodySystemSimulator::setVelocityOf(int i, Vec3 velocity)
 {
+	m_pRigidBodySystem[i].comVelocity = velocity;
 }
 
 void RigidBodySystemSimulator::IntegrateRigidBody(int i, float step)
 {
 	m_pRigidBodySystem[i].comPosition += step * m_pRigidBodySystem[i].comVelocity;
 	m_pRigidBodySystem[i].comVelocity += (step * m_externalForce / m_pRigidBodySystem[i].mass);
+	m_externalForce = Vec3(0, 0, 0);
 
-	m_pRigidBodySystem[i].orientation += step * 0.5f*Quat(m_pRigidBodySystem[i].angularVelocity.x, m_pRigidBodySystem[i].angularVelocity.y, m_pRigidBodySystem[i].angularVelocity.z, 0)*m_pRigidBodySystem[i].orientation;
+	Quat temp = Quat(m_pRigidBodySystem[i].angularVelocity.x, m_pRigidBodySystem[i].angularVelocity.y, m_pRigidBodySystem[i].angularVelocity.z, 0) * m_pRigidBodySystem[i].orientation;
+
+	m_pRigidBodySystem[i].orientation += (step * 0.5f) * temp;
+	float norm = m_pRigidBodySystem[i].orientation.norm();
+	m_pRigidBodySystem[i].orientation = (1 / norm) * m_pRigidBodySystem[i].orientation;
+
 	m_pRigidBodySystem[i].angularMomentum += step * torque;
 	Mat4 transposeRotation = m_pRigidBodySystem[i].orientation.getRotMat();
 	transposeRotation.transpose();
 	m_pRigidBodySystem[i].intertiaTensorInverse = m_pRigidBodySystem[i].orientation.getRotMat() * m_pRigidBodySystem[i].intertiaTensorInverse * transposeRotation;
 	m_pRigidBodySystem[i].angularVelocity = m_pRigidBodySystem[i].intertiaTensorInverse * m_pRigidBodySystem[i].angularMomentum;
-
+	torque = Vec3(0, 0, 0);
 	Vec3 pointVelocity = m_pRigidBodySystem[i].comVelocity + cross(m_pRigidBodySystem[i].angularVelocity, Vec3(-0.3f, -0.5f, -0.25f));
 
-	cout << "Angular Velocity after one Euler step: " << m_pRigidBodySystem[i].angularVelocity << "\n";
-	cout << "Linear Velocity at point (-0.3, -0.5, -0.25) after one Euler step: " << pointVelocity << "\n";
+	
+	if (m_iTestCase == 0) {
+		cout << "Angular Velocity after one Euler step: " << m_pRigidBodySystem[i].angularVelocity << "\n";
+		cout << "Linear Velocity at point (-0.3, -0.5, -0.25) after one Euler step: " << pointVelocity << "\n";
+	}
+	/*
+	else {
+		if (m_iTestCase == 1) {
+			cout << "Position of rigid body after one Euler step: " << m_pRigidBodySystem[i].comPosition << "\n";
+			cout << "Linear velocity of rigid body after one Euler step: " << m_pRigidBodySystem[i].comVelocity << "\n";
 
+		}
+	}*/
 
 }
